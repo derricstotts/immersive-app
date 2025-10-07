559034f2cded439925ad7a40af54065cd450d9db
devinjohnson1986 <devin.a.johnson.1986@gmail.com>
2025-10-07 09:59:22 -0600

    latest changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Test from "../components/test";
+import Design from "@/components/design";
 
 export default function Home() {
-    return <Test />;
+    return <Design />;
 }
